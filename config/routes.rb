489cba6017587ddb0c203118db40490bcef2e2c3
Jade Steffen <steffenjade@gmail.com>
2022-12-21 21:18:23 -0600

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "static_pages#landing_page"
 end
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem"
+    }
+  },
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ]
+}
