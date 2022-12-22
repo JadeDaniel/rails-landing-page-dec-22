7d190faf6eb7e90266c75a85a4252bfa953e22a4
Jade Steffen <steffenjade@gmail.com>
2022-12-21 21:33:18 -0600

    tailwind customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   theme: {
+    extend: {
+      colors: {
+        "monotelo-blue": "#2B689C",
+        "monotelo-dark-blue": "#134E6F",
+      }
+    },
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"]
+    },
     container: {
       center: true,
-      padding: "1rem"
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px"
+      }
     }
   },
   content: [
